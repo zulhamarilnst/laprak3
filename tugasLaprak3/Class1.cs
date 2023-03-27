bfa217ff17dce751aca2fd89a4d34d6764ccad50
zulhamarilnst <zlhmarilnst@students.amikom.ac.id>
2023-03-27 20:47:58 +0700

    ini tugas laprak minggu ke 3

---
+++ tugasLaprak3/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tugasLaprak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil1 = new Mobil();
+            
+            mobil1.warna = "Merah";
+            mobil1.jumlah_pintu = 2;
+            mobil1.merek = "Lamborgini";
+            mobil1.model = "AVENTADOR";
+            mobil1.tahun_keluaran = 2011;
+            mobil1.suara = "tid tid";
+            mobil1.kecepatan = "349 km/jam";
+
+            mobil1.gas();
+            mobil1.klakson();
+            mobil1.tampilkan_info();
+
+        }
+    }
+}
---
+++ tugasLaprak3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugasLaprak3
+{
+    public class Mobil
+    {
+        public string warna { get; set; }
+        public int jumlah_pintu { get; set; }
+        public string merek { get; set; }
+        public string model { get; set; }
+        public int tahun_keluaran { get; set; }
+        public string kecepatan { get; set; }
+        public string suara { get; set; }
+        
+        public void gas()
+        {
+            Console.WriteLine("==========================================================");
+            Console.WriteLine("Mobil " + model + " berjalan dengan kecepatan " + kecepatan);
+        }
+
+        public void klakson()
+        {
+            Console.WriteLine("suara kelakson : " + suara);
+        }
+
+        public void tampilkan_info()
+        {
+
+            Console.WriteLine("\"Mobil saya berwarna " + warna + ", merek " + merek + " model " + model + " keluaran tahun " + tahun_keluaran + " dengan jumlah pintu sebanyak " + jumlah_pintu + "\"");
+            Console.WriteLine("==========================================================");
+        }
+
+    }
+}
